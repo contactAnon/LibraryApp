fe47ed7c111f2f6e06c518a8a5732b6dd3956aa2
MapYh <145336421+MapYh@users.noreply.github.com>
2025-12-04 07:38:28 +0100

    Added build files and vercel files

--- BookApp.api/obj/Debug/net9.0/BookApp.api.AssemblyInfo.cs
+++ BookApp.api/obj/Debug/net9.0/BookApp.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApp.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2bd483a628e89b49e7a278bfae2b0189d31717")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d958d4e2989da01517403ad4f0c0349115592fd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApp.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApp.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
